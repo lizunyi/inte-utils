bd6ca79964bec73af0814d64e45f4fe9c8b1625f
lizunyi <4050660>
2022-09-23 08:48:37 +0800

    up

--- src/main/java/com/weaver/inte/Test.java
+++ src/main/java/com/weaver/inte/Test.java
@@ -1,6 +1,6 @@
 package com.weaver.inte;
 
-import com.weaver.inte.utils.ThreadWeaveCore;
+import com.weaver.inte.utils.WeaveThreadCore;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,7 +9,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println(null == null);
         System.out.println("开始");
-        ThreadWeaveCore a = new ThreadWeaveCore()
+        WeaveThreadCore a = new WeaveThreadCore()
                 .group(new Thread2(), new Thread1())
                 .group(new Thread2())
                 .addObject(new Example("你好"))
